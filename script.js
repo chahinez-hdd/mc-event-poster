b974dd92d945e248ae38acbf7b543e1d1c8428a1
chahinez-hdd <chahinez.haddad2005@gmail.com>
2024-10-30 13:33:00 +0100

    adding mgv agc

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ class EventPage {
                 </div>
             </div>
             <div class="icon-container">
-                <img src="MC WHITE.png" alt="MC WHITE Icon">
+                <img src="MC WHITE.png"  style="height: 110px; width: 110px; margin-bottom:-33px;" alt="MC WHITE Icon">
             </div>
         `;
         return div;
@@ -63,6 +63,20 @@ const events = [
         logoSrc: 'logo3.png',
         description: 'Micro Hack is a hackathon hosted by Micro Club during three days , where participants come together to tackle a real-world challenge. Teams brainstorm and develop creative solutions, racing against the clock to deliver their best ideas. At the end of the event, the top projects are recognized and rewarded for their innovation and impact. Micro Hack is a space for collaboration, creativity, and competition, where participants push their limits and turn bold ideas into reality.',
         backgroundGradient: 'linear-gradient(135deg, #54c0b2,  #16517b, #151515)',
+        borderColor: '#14e7cb'
+    }),
+    new EventPage({
+        title: 'AGC',
+        logoSrc: 'AGC-LOGO.png',
+        description: 'The Algeria Game Challenge (AGC) is an annual event that brings together game developers through a game development competition, along with workshops, conferences, and training sessions. Initially launched as the "XNA Game Challenge" in 2010 in partnership with Microsoft, it was renamed AGC in 2013. The competition involves teams of 2-5 participants creating innovative video games, judged on gameplay, storytelling, sound, and graphics. The two-day event includes technical conferences, final rounds of the competition, an exhibition for game studios, startups, and associations, and a Game Zone for attendees to enjoy featured games.',
+        backgroundGradient: 'linear-gradient(#2C74B3, #205295,  #144272, #0A2647)',
+        borderColor: '#14e7cb'
+    }),
+    new EventPage({
+        title: 'MGV',
+        logoSrc: 'MGV.svg',
+        description: 'MC GETS VISUAL (MGV) was an impactful event with a fresh concept, uniting creatives across fields like Graphic Design, UI/UX, 3D Design, and Motion Graphics. Its goal was to revive these disciplines, offering a space for networking, learning, and participation in carefully designed challenges. Born from a vision to foster creativity, MGV attracted both aspiring designers and industry professionals. The competition allowed teams to showcase their work, judged on innovation, storytelling, and visual quality. Over several days, workshops, talks, and an exhibition highlighted the latest trends, giving attendees the chance to connect, share ideas, and explore new opportunities.',
+        backgroundGradient: 'linear-gradient(  #FFBF78, #FF7D29)',
         borderColor: '#14e7cb'
     })
 ];
